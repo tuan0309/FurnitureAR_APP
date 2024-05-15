7badb8543eef45df5ceb932ae5f3ec15d235a0f5
tuan0309 <tuan0309220@gmail.com>
2024-05-15 13:41:28 +0700

    first commit

---
+++ Assets/Scripts/DataHandler.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class DataHandler : MonoBehaviour
+{
+    private GameObject furniture;
+
+    [SerializeField] private ButtonManager buttonPrefab;
+    [SerializeField] private GameObject buttonContainer;
+    [SerializeField] private List<Item> _items;
+    [SerializeField] private string label;
+
+    private int id = 0;
+
+    private static DataHandler instance;
+    public static DataHandler Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<DataHandler>();
+            }
+            return instance;
+        }
+
+    }
+    private async void Start()
+    {
+        _items = new List<Item>();
+        await Get(label);
+        CreateButtons();
+    }
+
+
+    void CreateButtons()
+    {
+        foreach (Item i in _items)
+        {
+            ButtonManager b = Instantiate(buttonPrefab, buttonContainer.transform);
+            b.ItemId = id;
+            b.ButtonTexture = i.itemImage;
+            id++;
+        }
+        buttonContainer.GetComponent<UIContentFitter>().Fit();
+    }
+    public void SetFurniture(int id)
+    {
+        if (id >= 0 && id < _items.Count)
+        {
+            furniture = _items[id].itemPrefab;
+        }
+        else
+        {
+            Debug.LogError("Invalid furniture id. Make sure the id is within the range of available items.");
+        }
+    }
+
+    public GameObject GetFurniture()
+    {
+        return furniture;
+    }
+
+    public async Task Get(string label)
+    {
+        var locations = await Addressables.LoadResourceLocationsAsync(label).Task;
+        foreach (var location in locations)
+        {
+            var obj = await Addressables.LoadAssetAsync<Item>(location).Task;
+            _items.Add(obj);
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class UIManager : MonoBehaviour
+{
+    private GraphicRaycaster raycaster;
+    private PointerEventData pData;
+    private EventSystem eventSystem;
+
+    public Transform selectionPoint;
+    private static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<UIManager>();
+            }
+            return instance;
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        raycaster = GetComponent<GraphicRaycaster>();
+        eventSystem = GetComponent<EventSystem>();
+        pData = new PointerEventData(eventSystem);
+        pData.position = selectionPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public bool OnEntered(GameObject button)
+    {
+        //Create a list of Raycast Results
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        //Raycast using the Graphics Raycaster and mouse click position
+        raycaster.Raycast(pData, results);
+
+        //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject == button)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
